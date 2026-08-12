fce20f83d3105d914ea298bbcfc6b0c8ebea555f
pawan <glbitm3@gmail.com>
2026-08-12 15:45:39 +0530

    Time: 3 ms (86.05%) | Memory: 43.2 MB (73.37%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack <Character> stk=new Stack<>();
+        for(int i=0;i<s.length();i++){
+            char ch=s.charAt(i);
+            if(ch=='(' || ch=='[' || ch=='{'){
+                stk.push(ch);
+
+            }else{
+                if(stk.isEmpty()){
+                    return false;
+                }
+                if((ch==')' && stk.peek()=='(') || (ch==']' && stk.peek()=='[') || (ch=='}' && stk.peek()=='{')){
+                    stk.pop();
+
+                }else{
+                    return false;
+                }
+            }
+        }
+        if(stk.isEmpty()){
+            return true;
+        }else{
+            return false;
+        }
+        
+    }
+}
