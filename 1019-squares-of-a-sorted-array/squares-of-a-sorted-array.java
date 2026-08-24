1c6fdf41933efe9d295ab1659ef465c1cce47296
pawan <glbitm3@gmail.com>
2026-08-24 21:54:11 +0530

    Time: 1 ms (100.00%) | Memory: 47.3 MB (53.17%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.java
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.java
@@ -1,23 +1,21 @@
 class Solution {
-    public int[] sortedSquares(int[] nums){
-        for(int i=0;i<nums.length;i++){
-            nums[i]=(int)Math.pow(nums[i],2);
-        }
-        for (int i = 0; i < nums.length - 1; i++) {
-            boolean swap = false;
-            for (int j = 0; j < nums.length - 1 - i; j++) {
-                if (nums[j] > nums[j + 1]) {
-                    int temp = nums[j];
-                    nums[j] = nums[j + 1];
-                    nums[j + 1] = temp;
-                    swap = true;
-                }
+    public int[] sortedSquares(int[] nums) {
+        int[] ans = new int[nums.length];
+        int i = 0;
+        int j = nums.length - 1;
+        int k = nums.length - 1;
 
+        while (i <= j) {
+            if (Math.abs(nums[i]) > Math.abs(nums[j])) {
+                ans[k] = nums[i] * nums[i];
+                i++;
+            } else {
+                ans[k] = nums[j] * nums[j];
+                j--;
             }
-            if (swap == false) {
-                break;
-            }
+            k--;
         }
-        return nums;    
+
+        return ans;
     }
 }
